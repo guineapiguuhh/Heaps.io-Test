4cd3d525c8e5265e3724bb8cf4c6ebbc3b810a52
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2025-07-16 20:58:19 -0300

    v2

--- src/Main.hx
+++ src/Main.hx
@@ -4,95 +4,119 @@ import h2d.Bitmap;
 import h2d.Text;
 import hxd.res.DefaultFont;
 import hxd.App;
-// import h2d.Interactive;
 import h2d.col.Point;
 import hxd.Key;
 import h2d.col.RoundRect;
 
-class Main extends App {
-	var obj:Object;
-	var debugTxt:Text;
+class Main extends App 
+{
+	var haxeObj:Object;
+	var haxeBitmap:Bitmap;
 
+	var haxeVelocityX:Float = 0;
+	var haxeVelocityY:Float = 0;
+
+	var scoreTxt:Text;
 	var score:Int = 0;
 
-	static var maxTime:Int = 200;
-
+	static var maxTime:Int = 1000;
+	var timeTxt:Text;
 	var time:Int = maxTime;
+	var updateTime:Bool = false;
 
-	override function init() {
-		debugTxt = new Text(DefaultFont.get());
-		debugTxt.scale(5);
-		debugTxt.text = "Score: 0";
-		debugTxt.text += '\nTime: 0';
+	static var gameWidth:Float = 1366;
+	static var gameHeight:Float = 768;
 
-		obj = new Object(s2d);
-		obj.x = 600;
-		obj.y = 600;
-		obj.alpha = 0.6;
+	var hardMode(default, set):Bool = false;
 
+	function set_hardMode(v:Bool):Bool
+	{
+		hardMode = v;
+		scoreTxt.textColor = hardMode ? 0xFF0000 : 0xFFFFFF;
+		if (!hardMode) haxeVelocityX = haxeVelocityY = 0;
+		return hardMode;
+	}
+
+	function reset()
+	{
+		score = 0;
+		scoreTxt.text = "Score: -";
+		timeTxt.text = "Time left: -";
+		time = maxTime;
+		updateTime = false;
+		hardMode = false;
+
+		haxeObj.x = gameWidth/2;
+		haxeObj.y = gameHeight/2;
+	}
+
+	override function init() 
+	{
+		haxeObj = new Object(s2d);
 		var tile = Res.images.hxlogo.toTile();
 		tile = tile.center();
-		var mybitmap = new Bitmap(tile, obj);
+		haxeBitmap = new Bitmap(tile, haxeObj);
 
-		// i hate you 😡!
-		/*
-			var haxeThing = new Interactive(200, 200, obj);
-			haxeThing.onOver = function(event:hxd.Event) {
-				obj.alpha = 1;
-			}
-			haxeThing.onOut = function(event:hxd.Event) {
-				obj.alpha = 0.6;
-			}
-			haxeThing.onClick = function(event:hxd.Event) {
-				score++;
-				debugTxt.text = "Score: " + score;
-				obj.setScale(1.5);
-				obj.x = hxd.Math.random(1280);
-				obj.y = hxd.Math.random(1080);
-			}
-		 */
+		scoreTxt = new Text(DefaultFont.get(), s2d);
+		scoreTxt.x = gameWidth/2;
+		scoreTxt.textAlign = Center;
+		scoreTxt.scale(3.5);
 
-		s2d.addChild(debugTxt);
+		timeTxt = new Text(DefaultFont.get(), s2d);
+		timeTxt.scale(2.5);
+
+		reset();
 	}
 
-	override function update(dt:Float) {
-		debugTxt.textColor = score >= 50 ? 0xFF0000 : 0xFFFFFF;
+	override function update(dt:Float) 
+	{
+		if (updateTime)
+		{
+			time--;
+			timeTxt.text = "Time left: " + time;
+			if (time <= 0) reset();
+		}
+		updateHaxeObj(dt);
+	}
 
-		var shit = new RoundRect(obj.x, obj.y, 300, 300, obj.rotation);
-		var mousething = new Point(s2d.mouseX, s2d.mouseY);
+	function updateHaxeObj(dt:Float) 
+	{
+		var objHitbox = new RoundRect(haxeObj.x, haxeObj.y, 228, 228, haxeObj.rotation);
+		var mousePos = new Point(s2d.mouseX, s2d.mouseY);
 
-		time--;
-		if (shit.inside(mousething)) {
-			obj.alpha = 1;
-			if (Key.isReleased(Key.MOUSE_LEFT)) {
+		if (haxeObj.scaleX != 1) haxeObj.setScale(hxd.Math.lerp(haxeObj.scaleX, 1, 6 * dt));
+
+		if (objHitbox.inside(mousePos)) 
+		{
+			haxeObj.alpha = 1;
+			if (Key.isPressed(Key.MOUSE_LEFT)) 
+			{
+				haxeObj.setScale(0.9);
+				haxeObj.x = hxd.Math.random(gameWidth);
+				haxeObj.y = hxd.Math.random(gameHeight);
+
+				scoreTxt.text = "Score: " + score;
 				score++;
-				obj.setScale(1.5);
-				obj.x = hxd.Math.random(980); // screen shit - width i think
-				obj.y = hxd.Math.random(780); // screen fortnine - height i think
-				if (maxTime <= 50) {
-					maxTime -= 5
-				}
 				time = maxTime;
+				updateTime = true;
+
+				hardMode = score >= 25;
 			}
-		} else {
-			obj.alpha = 0.6;
+		} 
+		else haxeObj.alpha = 0.6;
+
+		if (hardMode)
+		{
+			haxeObj.x += haxeVelocityX*dt;
+			haxeObj.y += haxeVelocityY*dt;
+
+			if (haxeObj.x >= gameWidth || haxeObj.x <= 0) haxeVelocityX *= -1;
+			if (haxeObj.y >= gameHeight || haxeObj.y <= 0) haxeVelocityY *= -1;
 		}
-
-		if (obj.scaleX != 1)
-			obj.setScale(hxd.Math.lerp(obj.scaleX, 1, 0.2));
-
-		if (time < 0) {
-			score = hxd.Math.floor(score / 1.5);
-			if (maxTime <= 50)
-				maxTime = hxd.Math.floor(maxTime / 1.2);
-			time = maxTime;
-		}
-
-		debugTxt.text = "Score: " + score;
-		debugTxt.text += '\nTime: ' + time;
 	}
 
-	static function main() {
+	static function main() 
+	{
 		Res.initEmbed();
 		new Main();
 	}
--- utils/compile.bat
+++ utils/compile.bat
@@ -1,14 +1,7 @@
 @echo off
 cd ..
 
-echo Deleting the Old Files...
-del hello.js
-del hello.js.map
-
 echo Compiling the Application...
 haxe compile.hxml
-
-echo Starting the Application...
-start index.html
 
 pause
